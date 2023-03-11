3038215e15309a0ea861c286fcc85bbd005382ce
rodevilst <rosolomatin@gmail.com>
2023-03-11 18:07:37 +0200

    init

--- src/main/java/finalproject/FinalApplication.java
+++ src/main/java/finalproject/FinalApplication.java
@@ -3,12 +3,13 @@ package finalproject;
 import finalproject.configs.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @Import(SwaggerConfig.class)
-public class FinalApplication {
+public class FinalApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FinalApplication.class, args);
